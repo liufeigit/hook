d68ce2397d5770c6fe231b75a4ecfcff2b282efd
liufeigit <woaicplusplus@gmail.com>
2014-10-22 14:14:35 +0800

    libA.dylib

--- A.c
+++ A.c
@@ -1,17 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <dlfcn.h>
-// libA.dylib
+#include "A.h"
 
-typedef struct A{
-	int num;
-	char *name;
-	void (*callBack)(int );
-	void *id;
-} A;
 
-void testA();
+static void _testA(){
+	printf("_testA......\n");
+}
 
-void callBack(int );
+void testA(){
+	_testA();
+	printf("testA......\n");
+}
 
-A *initA();
+void callBack(int num){
+	printf("callBack is %d\n",num );
+}
+
+A *initA(){
+
+	int size;
+	size=sizeof(A);
+	A *a1=(A *)malloc(size*1);
+	a1->name="asdf";
+	a1->num=12;
+	a1->callBack=callBack;
+	a1->id=(void *)testA; // 测试使用
+
+	return a1;
+}
+
+A ATable={130,"table",callBack,testA};
