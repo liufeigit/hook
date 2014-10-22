15dbe5d677bc47adfb1dc7fc8c632da9dd126668
liufeigit <woaicplusplus@gmail.com>
2014-10-22 14:55:23 +0800

    Update hook.c

--- hook.c
+++ hook.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2014, buluobang , Inc.
+// Copyright (c) 2014, 部落邦 , Inc.
 // All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -20,7 +20,7 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// author @liufei
+// author @刘飞
 
 #include <objc/runtime.h>
 #include <mach-o/dyld.h>
